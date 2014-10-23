17874b9d533a93bfc71806847d799bbb2b1cfc12
eeg_imac <ray@Rays-iMac.local>
2014-10-23 17:46:46 -0300

    some tweaking of eyewink

--- eyelinkProcess.py
+++ eyelinkProcess.py
@@ -1,11 +1,13 @@
 # class dummyQTo:
 # 	def empty(self):
 # 		return True
-# 
+
+
 # class dummyQFrom:
 # 	def put(self,message):
 # 		print message
-# 
+
+
 # qTo = dummyQTo()
 # qFrom = dummyQFrom()
 
@@ -13,6 +15,7 @@
 # windowPosition = [0,0]
 # stimDisplayRes = [1920,1080]
 # eyelinkIP = '100.1.1.1'
+# edfFileName = 'ssior.edf'
 # edfPath = './_Data/'
 # saccadeSoundFile = '_Stimuli/stop.wav'
 # blinkSoundFile = '_Stimuli/stop.wav'
@@ -26,6 +29,9 @@ import shutil
 try:
 	import appnope
 	appnope.nope()
+except:
+	pass
+
 
 sdl2.SDL_Init(sdl2.SDL_INIT_VIDEO)
 window = sdl2.ext.Window("eyelink",size=windowSize,position=windowPosition,flags=sdl2.SDL_WINDOW_SHOWN)
@@ -43,13 +49,17 @@ sdl2.sdlmixer.Mix_OpenAudio(44100, sdl2.sdlmixer.MIX_DEFAULT_FORMAT, 2, 1024)
 class Sound:
 	def __init__(self, fileName):
 		self.sample = sdl2.sdlmixer.Mix_LoadWAV(sdl2.ext.compat.byteify(fileName, "utf-8"))
+		self.started = False
 	def play(self):
 		self.channel = sdl2.sdlmixer.Mix_PlayChannel(-1, self.sample, 0)
+		self.started = True
 	def stillPlaying(self):
-		if sdl2.sdlmixer.Mix_Playing(self.channel):
-			return True
-		else:
-			return False
+		if self.started:
+			if sdl2.sdlmixer.Mix_Playing(self.channel):
+				return True
+			else:
+				self.started = False
+				return False
 
 saccadeSound = Sound(saccadeSoundFile)
 blinkSound = Sound(blinkSoundFile)
@@ -82,9 +92,10 @@ eyelink.setLinkEventFilter("SACCADE,BLINK")
 eyelink.openDataFile(edfFileName)
 eyelink.sendCommand("screen_pixel_coords =  0 0 %d %d" %(stimDisplayRes[0],stimDisplayRes[1]))
 eyelink.sendMessage("DISPLAY_COORDS  0 0 %d %d" %(stimDisplayRes[0],stimDisplayRes[1]))
-eyelink.sendCommand("saccade_velocity_threshold = 35")
-eyelink.sendCommand("saccade_acceleration_threshold = 9500")
+eyelink.sendCommand("saccade_velocity_threshold = 60")
+eyelink.sendCommand("saccade_acceleration_threshold = 19500")
 eyelink.doTrackerSetup()
+eyelink.startRecording(1,1,1,1) #this retuns immediately takes 10-30ms to actually kick in on the tracker
 
 while True:
 	sdl2.SDL_PumpEvents()
@@ -102,6 +113,10 @@ while True:
 	if (eyeData==pylink.STARTSACC) or (eyeData==pylink.STARTBLINK):
 		eyeEvent = eyelink.getFloatData()
 		if isinstance(eyeEvent,pylink.StartSaccadeEvent):
-			print 'Saccade started'
+			# print 'Saccade started'
+			if (not saccadeSound.stillPlaying()) and (not blinkSound.stillPlaying()):
+				saccadeSound.play()
 		elif isinstance(eyeEvent,pylink.StartBlinkEvent):
-			print 'Blink started'
+			# print 'Blink started'
+			if (not saccadeSound.stillPlaying()) and (not blinkSound.stillPlaying()):
+				blinkSound.play()
